646fc48145e0b682d5a4ec78d7b933e460b8501e
vinay <vinaybkrishna10@gmail.com>
2025-04-21 19:13:15 +0530

    Commmmiting mmy first commit

--- main.tf
+++ main.tf
@@ -23,6 +23,8 @@ output "data" {
     value = "result from other file -${var.data.name} "
 }
 
+variable "data2" {}
+
 
 output "data2" {
     value = "result from other file two data -${var.data2.name} "
