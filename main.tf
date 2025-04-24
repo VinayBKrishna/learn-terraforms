b7ef26a8ec6c4974303b55c8ade6984e3e087b74
vinay <vinaybkrishna10@gmail.com>
2025-04-24 21:02:46 +0530

    Commmmiting mmy first commit

--- main.tf
+++ main.tf
@@ -1,31 +1,4 @@
-variable "x" {
-    default = 10
-
+resource "null_resource" "testing" {
+    count = 10
 }
 
-variable "y" {
-    default = 11
-}
-
-output "x" {
-    value = var.x
-
-}
-
-output "y" {
-    value = var.y
-
-}
-
-variable "data" {}
-
-output "data" {
-    value = "result from other file -${var.data.name} "
-}
-
-variable "data2" {}
-
-
-output "data2" {
-    value = "result from other file two data -${var.data2.name} "
-}
