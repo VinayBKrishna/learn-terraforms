ca97cd3eeec3b98dd1a5cfda582bac8c14927a63
vinay <vinaybkrishna10@gmail.com>
2025-04-25 16:10:33 +0530

    Commmmiting mmy first commit

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ resource "null_resource" "testing" {
     count = 10
 }
 
+variable "list" {
+    default = [
+        1,2,3,4]
+}
+
+output "list_output" {
+    value = { for index,value in var.list: index => values }
+}
