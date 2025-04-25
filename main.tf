f6b4f62225e5537447bb0cd1e39bf46e9f20edca
vinay <vinaybkrishna10@gmail.com>
2025-04-25 16:59:39 +0530

    Commmmiting mmy first commit

--- main.tf
+++ main.tf
@@ -13,4 +13,8 @@ module "text_print3" {
     text_to_print = "this is 2nd sentence"
 }
 
+module "text_print4" {
+    source = "./modules"
+    text_to_print = "this is someting new to occure and alast the worldddddddddddd"
+}
 
