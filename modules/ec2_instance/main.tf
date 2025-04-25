cb6e7c11738e95facad1220a4aa3bf89419da7b3
vinay <vinaybkrishna10@gmail.com>
2025-04-25 21:02:23 +0530

    Commmmiting mmy first commit

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  vpc_security_group_ids = var.vpc_security_group_ids
+  # vpc_security_group_ids = var.vpc_security_group_ids
 
 
   tags = {
